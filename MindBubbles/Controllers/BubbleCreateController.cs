9bdc0a17993cf9bea2a7fb7f978f0aa5c7bbde00
Lukas Vaclavek <poortrolls@gmail.com>
2018-09-03 13:25:05 +0200

    First stage finsihed bubbles are made from text atm

---
+++ MindBubbles/Controllers/BubbleCreateController.cs
@@ -0,0 +1,40 @@
+﻿namespace MindBubbles.Controllers
+{
+    using MindBubbles.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class BubbleCreateController : Controller
+    {
+        private const string BubbleString = "bublina1*bublina2*bublina3*bublina4";
+
+        public string BaseString;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BubbleCreate(BubbleCreateModel bubbleCreateModel)
+        {
+
+            return View(CreateBubble(bubbleCreateModel));
+
+
+        }
+        public BubbleCreateModel CreateBubble(BubbleCreateModel bubbleCreateModel)
+        {
+            string[] converted;
+            converted = bubbleCreateModel.BaseString.Split('*');
+            bubbleCreateModel.BaseStringList = converted;
+            return bubbleCreateModel;
+        }
+
+
+        public ActionResult InputSource()
+        {
+            return View();
+        }
+    }
+}
---
+++ MindBubbles/Models/BubbleCreateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MindBubbles.Models
+{
+
+    public class BubbleCreateModel
+    {
+        public string BaseString { get; set; }
+        public string[] BaseStringList { get; set; }
+
+    }
+}
