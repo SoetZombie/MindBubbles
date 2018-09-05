80f9a0e82b8f42ae6ed6877d327cb882f4fa234a
Lukas Vaclavek <poortrolls@gmail.com>
2018-09-05 14:17:53 +0200

    Finished parsing from data string atm working on lines connecting the bubbles

--- MindBubbles/Controllers/BubbleCreateController.cs
+++ MindBubbles/Controllers/BubbleCreateController.cs
@@ -20,7 +20,7 @@
         [HttpPost]
         public ActionResult BubbleCreate(InputStringModel inputStringModel)
         {
-            inputStringModel.InputString = "1.  Rozvoj dopravy a dopravní infrastruktury obce | 2. Životní prostředí a území | 3. Územní rozvoj obce | 1.1 Zlepšit podmínky | 2.1 Zlepšit stav | 3.1 Zlepšit psa | 1.2 Zlepsit mozek | 2.2 Zlepsit karla 	 3.2 Zlepsit kone | 1.3 Dobudovat | 2.3 Mozek | 3.3 nevim uz";
+            inputStringModel.InputString = "1.  Rozvoj dopravy a dopravní infrastruktury obce | 2. Životní prostředí a území | 3. Územní rozvoj obce | 1.1 Zlepšit podmínkysv ramci rozvoje oblastního zatizeni a take odpadu | 2.1 Zlepšit stav | 3.1 Zlepšit psa | 1.2 Zlepsit mozek | 2.2 Zlepsit karla | 3.2 Zlepsit kone | 1.3 Dobudovat | 2.3 Mozek a ruka jdou na nakup myslim si ze maji malo kebabu a tak se bojim aby jim nebylo zle | 3.3 nevim uz";
             
             return View(CreateBubbles(inputStringModel));
 
@@ -46,7 +46,7 @@
                 var bubbleToList = new BubbleCreateModel()
                 {
                     PlainTextInCell = transformedText,
-                    OrderNumber = number
+                    OrderNumber = " "+number
 
                 };
 
@@ -63,8 +63,34 @@
             };
 
             
-            return listToReturn;
+            return GenerateLists(listToReturn);
          
+        }
+
+        public BubblesList GenerateLists (BubblesList bubblesList )
+        {
+            var regex1 = new Regex(@" 1+(\.\d+)+");
+            var regex2 = new Regex(@" 2+(\.\d+)+");
+            var regex3 = new Regex(@" 3+(\.\d+)+");
+            var regex4 = new Regex(@" 4+(\.\d+)+");
+            var regex5 = new Regex(@" 5+(\.\d+)+");
+            var regex6 = new Regex(@" 6+(\.\d+)+");
+
+            bubblesList.AllLists = new List<List<BubbleCreateModel>>();
+            bubblesList.List1 = bubblesList.AllBubbleData.Where(a => regex1.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 1")).ToList();
+            bubblesList.List2 = bubblesList.AllBubbleData.Where(a => regex2.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 2")).ToList();
+            bubblesList.List3 = bubblesList.AllBubbleData.Where(a => regex3.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 3")).ToList();
+            
+            //bubblesList.List4 = bubblesList.AllBubbleData.Where(a => regex4.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 4")).ToList();
+           // bubblesList.List5 = bubblesList.AllBubbleData.Where(a => regex5.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 5")).ToList();
+            //bubblesList.List6 = bubblesList.AllBubbleData.Where(a => regex6.IsMatch(a.OrderNumber) || a.OrderNumber.Equals(" 6")).ToList();
+
+            bubblesList.AllLists.Add(bubblesList.List1);
+            bubblesList.AllLists.Add(bubblesList.List2);
+            bubblesList.AllLists.Add(bubblesList.List3);
+         
+            return bubblesList;
+
         }
 
 
--- MindBubbles/Models/BubblesList.cs
+++ MindBubbles/Models/BubblesList.cs
@@ -8,5 +8,19 @@ namespace MindBubbles.Models
     public class BubblesList
     {
         public List<BubbleCreateModel> AllBubbleData { get; set; }
+
+        public List<BubbleCreateModel> List1{ get; set; }
+
+        public List<BubbleCreateModel> List2 { get; set; }
+
+        public List<BubbleCreateModel> List3 { get; set; }
+
+        public List<BubbleCreateModel> List4 { get; set; }
+
+        public List<BubbleCreateModel> List5 { get; set; }
+
+        public List<BubbleCreateModel> List6 { get; set; }
+
+        public List<List<BubbleCreateModel>> AllLists { get; set; }
     }
 }
